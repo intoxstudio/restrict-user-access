8a77f4ef88b0907a27a79d59da2cc7bb9d39cc69
Joachim Jensen <jv@intox.dk>
2016-01-12 17:52:14 -0800

    api to get: - user roles - user levels - user level start - user level expiry - is user level expired - has user level - level by name

---
+++ api/level.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * @package Restrict User Access
+ * @copyright Joachim Jensen <jv@intox.dk>
+ * @license GPLv3
+ */
+if (!defined('ABSPATH')) {
+	header('Status: 403 Forbidden');
+	header('HTTP/1.1 403 Forbidden');
+	exit;
+}
+
+/**
+ * API to get user roles
+ *
+ * @since  0.9
+ * @param  int   $user_id
+ * @return array
+ */
+function rua_get_user_roles($user_id) {
+	return RUA_App::instance()->level_manager->get_user_roles($user_id);
+}
+
+/**
+ * API to get user levels
+ *
+ * @since  0.9
+ * @param  int     $user_id
+ * @param  boolean $hierarchical
+ * @param  boolean $synced_roles
+ * @param  boolean $include_expired
+ * @return array
+ */
+function rua_get_user_levels(
+	$user_id = null,
+	$hierarchical = true,
+	$synced_roles = true,
+	$include_expired = false) {
+	return RUA_App::instance()->level_manager->get_user_levels($user_id,$hierarchical,$synced_roles,$include_expired);
+}
+
+/**
+ * API to get user level start time
+ *
+ * @since  0.9
+ * @param  int  $user_id
+ * @param  int  $level_id
+ * @return int
+ */
+function rua_get_user_level_start($user_id = null,$level_id) {
+	return RUA_App::instance()->level_manager->get_user_level_start($user_id,$level_id);
+}
+
+/**
+ * API to get user level expiry time
+ *
+ * @since  0.9
+ * @param  int  $user_id
+ * @param  int  $level_id
+ * @return int
+ */
+function rua_get_user_level_expiry($user_id = null, $level_id) {
+	return RUA_App::instance()->level_manager->get_user_level_expiry($user_id,$level_id);
+}
+
+/**
+ * API to check if user level is expired
+ *
+ * @since  0.9
+ * @param  int  $user_id
+ * @param  int  $level_id
+ * @return boolean
+ */
+function rua_is_user_level_expired($user_id = null, $level_id) {
+	return RUA_App::instance()->level_manager->is_user_level_expired($user_id,$level_id);
+}
+
+/**
+ * API to check if user has level
+ *
+ * @since  0.9
+ * @param  int  $user_id
+ * @param  int  $level_id
+ * @return boolean
+ */
+function rua_has_user_level($user_id,$level_id) {
+	return RUA_App::instance()->level_manager->has_user_level($user_id,$level_id);
+}
+
+/**
+ * API to get level by name
+ *
+ * @since  0.9
+ * @param  string  $name
+ * @return int
+ */
+function rua_get_level_by_name($name) {
+	return RUA_App::instance()->level_manager->get_level_by_name($name);
+}
+
+//eol
