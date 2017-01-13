f87fb173897d174a0b446108a491c0244a79bfda
Joachim Jensen <jv@intox.dk>
2017-01-13 14:44:29 +0100

    fix upgrade db upgrade script

--- db_updates.php
+++ db_updates.php
@@ -7,11 +7,11 @@
 
 if(is_admin()) {
 	$rua_db_updater = new WP_DB_Updater("rua_plugin_version",RUA_App::PLUGIN_VERSION);
-	$rua_db_updater->register_version_update("0.4","rua_db_userlevel_date");
+	$rua_db_updater->register_version_update("0.4","rua_update_to_04");
 	$rua_db_updater->register_version_update('0.13','rua_update_to_013');
 
 	/**
-	 * Update to version0.13
+	 * Update to version 0.13
 	 * Inherit condition exposure from level
 	 * Remove level exposure
 	 *
@@ -47,7 +47,7 @@ if(is_admin()) {
 	 * @since  0.4
 	 * @return boolean
 	 */
-	function rua_db_userlevel_date() {
+	function rua_update_to_04() {
 		global $wpdb;
 
 		//Get levels by umeta id and level id
@@ -59,15 +59,15 @@ if(is_admin()) {
 
 		$level_dates = $wpdb->get_results("SELECT user_id,meta_key,meta_value FROM $wpdb->usermeta WHERE meta_key LIKE '_ca_level_%'");
 		foreach($level_dates as $level_date) {
-			$level_date_level_id = str_replace("_ca_level_", "", $level_date->meta_key);
+			$level_date_metaid = str_replace("_ca_level_", "", $level_date->meta_key);
 			//Check if date exists by level umeta id (old store)
 			//If so, move it to new
-			if(isset($levels_by_metaid[$level_date_level_id])) {
-				update_user_meta($level_date->user_id,"_ca_level_".$levels_by_metaid[$level_date_level_id]->meta_value,$level_date->meta_value,true);
+			if(isset($levels_by_metaid[$level_date_metaid])) {
+				update_user_meta($level_date->user_id,"_ca_level_".$levels_by_metaid[$level_date_metaid]->meta_value,$level_date->meta_value,true);
 			}
 			//Check if date exists by level id (new store)
 			//If not, delete it
-			if(!isset($levels_by_id[$level_date_level_id])) {
+			if(!isset($levels_by_id[$level_date_metaid])) {
 				delete_user_meta($level_date->user_id,$level_date->meta_key);
 			}
 		
