1e47f5bd3e98dead3b14d8e3d43cd2602af85e3f
Joachim Jensen <joachim@dev.institute>
2022-03-02 22:26:21 -0800

    reinstate role sync option. remove alert

--- view/meta_box_members.php
+++ view/meta_box_members.php
@@ -11,10 +11,7 @@ $list_members->prepare_items();
 
 $role = RUA_App::instance()->level_manager->metadata()->get('role')->get_data($post->ID, true);
 
-if($role !== '') {
-    echo '<div style="background-color:#fcf0f1;padding: 8px 20px;border: 1px solid #c3c4c7;border-left-color: #d63638;border-left-width: 4px;">Synchronized Role option will be removed in a future version. Please use the Login State automation instead.</div>';
-    RUA_Level_Edit::form_field('role');
-}
+RUA_Level_Edit::form_field('role');
 
 echo '<select class="js-rua-user-suggest" multiple="multiple" name="users[]"></select>';
 $list_members->display();
