1875bcca0e88cad437f65f25d014753af808a62c
Joachim Jensen <joachim@dev.institute>
2023-05-13 00:40:09 -0700

    [RUA-40] display search form

--- view/meta_box_members.php
+++ view/meta_box_members.php
@@ -8,5 +8,12 @@
 
 $list_members = new RUA_Members_List();
 $list_members->prepare_items();
+
+if (isset($_GET['s']) && strlen($_GET['s'])) {
+    /* translators: %s: search keywords */
+    printf(' <span class="subtitle">' . __('Search results for &#8220;%s&#8221;') . '</span>', get_search_query());
+}
+
+$list_members->search_box('Search Members', 'post');
 $list_members->display();
 echo '<p></p><select class="js-rua-user-suggest" multiple="multiple" name="users[]"></select>';
