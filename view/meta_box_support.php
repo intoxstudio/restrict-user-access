4137d3c7ad39c1e4f127dca6e6a144da930018b1
Joachim Jensen <joachim@dev.institute>
2023-12-15 17:50:23 -0800

    docblock

--- view/meta_box_support.php
+++ view/meta_box_support.php
@@ -3,7 +3,7 @@
  * @package Restrict User Access
  * @author Joachim Jensen <joachim@dev.institute>
  * @license GPLv3
- * @copyright 2022 by Joachim Jensen
+ * @copyright 2023 by Joachim Jensen
  */
 
 $rua_fs = rua_fs();
