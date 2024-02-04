fea8342fae3ccea888d6805b78659e8eb7533ae3
Joachim Jensen <joachim@dev.institute>
2024-02-03 16:19:26 -0800

    [RUA-48] intercept rest api call

--- app.php
+++ app.php
@@ -153,6 +153,8 @@ final class RUA_App
             'cas/user_visibility',
             [$this,'sidebars_check_levels']
         );
+
+        add_filter('rest_authentication_errors', [$this, 'rest_api_access']);
     }
 
     public function ensure_wpca_loaded()
@@ -663,5 +665,76 @@ final class RUA_App
                 $automator->add_callback();
             }
         }
+    }
+
+    public function rest_api_access($result)
+    {
+        //bail if auth has been handled elsewhere
+        if ($result === true || is_wp_error($result)) {
+            return $result;
+        }
+
+        if (rua_get_user()->has_global_access()) {
+            return $result;
+        }
+
+        if (!get_option('rua_rest_api_access', 1)) {
+            return $result;
+        }
+
+        //Contributor is the lowest role that should have access,
+        //since they can see content in admin area
+        if (current_user_can('edit_posts')) {
+            return $result;
+        }
+
+        $restricted = [
+            '/wp/v2/search' => true,
+            '/wp/v2/users'  => true
+        ];
+
+        $ignored_post_types = [
+            'nav_menu_item'    => true,
+            'wp_block'         => true,
+            'wp_template'      => true,
+            'wp_template_part' => true,
+            'wp_navigation'    => true
+        ];
+        foreach (get_post_types(['show_in_rest' => true], 'objects') as $post_type) {
+            if (empty($post_type->rest_base)) {
+                continue;
+            }
+            if (isset($ignored_post_types[$post_type->name])) {
+                continue;
+            }
+            $restricted['/' . $post_type->rest_namespace . '/' . $post_type->rest_base] = true;
+        }
+        $ignored_taxonomies = [
+            'menu' => true,
+        ];
+        foreach (get_taxonomies(['show_in_rest' => true], 'objects') as $taxonomy) {
+            if (empty($taxonomy->rest_base)) {
+                continue;
+            }
+            if (isset($ignored_taxonomies[$post_type->name])) {
+                continue;
+            }
+            $restricted['/' . $taxonomy->rest_namespace . '/' . $taxonomy->rest_base] = true;
+        }
+
+        global $wp;
+
+        $route = $wp->query_vars['rest_route'];
+        $route = preg_replace('/(\/\d+)$/', '', $route, 1);
+
+        if (!isset($restricted[$route])) {
+            return $result;
+        }
+
+        return new WP_Error(
+            'rest_forbidden',
+            __('Sorry, you are not allowed to do that.'),
+            ['status' => rest_authorization_required_code()]
+        );
     }
 }
