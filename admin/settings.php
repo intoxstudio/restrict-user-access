67328d217336c80f5fbd9f08cf0f8abb503865a7
Joachim Jensen <joachim@dev.institute>
2024-02-18 23:34:26 -0800

    [RUA-48] Setting - REST API Content Protection (#48)
    * [RUA-48] settings screen cleanup. new setting type
    
    * [RUA-48] setting: rest api content protection
    
    * [RUA-48] intercept rest api call
    
    * [RUA-48] label

--- admin/settings.php
+++ admin/settings.php
@@ -10,6 +10,7 @@ defined('ABSPATH') || exit;
 
 final class RUA_Settings_Page extends RUA_Admin
 {
+    const PREFIX = 'rua_';
     /**
      * Settings slug
      * @var string
@@ -25,6 +26,7 @@ final class RUA_Settings_Page extends RUA_Admin
     /**
      * Settings prefix
      * @var string
+     * @deprecated
      */
     private $prefix = 'rua-';
 
@@ -152,6 +154,7 @@ final class RUA_Settings_Page extends RUA_Admin
      */
     public function add_scripts_styles()
     {
+        WPCACore::enqueue_scripts_styles(RUA_App::TYPE_RESTRICT);
     }
 
     public function init_settings()
@@ -162,44 +165,68 @@ final class RUA_Settings_Page extends RUA_Admin
                 'title'    => __('General', 'restrict-user-access'),
                 'callback' => '',
                 'fields'   => []
+            ],
+            'security' => [
+                'name'     => 'security',
+                'title'    => __('Security', 'restrict-user-access'),
+                'callback' => '',
+                'fields'   => []
             ]
         ];
 
+        $levels = [
+            0 => __('-- None --')
+        ];
+        foreach (RUA_App::instance()->get_levels() as $id => $level) {
+            $levels[$level->ID] = $level->post_title;
+        }
+
         $this->settings['general']['fields'][] = [
-            'name'     => 'registration-level',
+            'name'     => 'rua-registration-level',
             'title'    => __('New User Default Level', 'restrict-user-access'),
-            'callback' => [$this,'dropdown_levels'],
+            'callback' => [$this,'dropdown'],
             'args'     => [
-                'label_for' => $this->prefix . 'registration-level'
+                'options' => $levels
             ]
         ];
 
         $default_role = get_option('default_role');
         $roles = get_editable_roles();
         $this->settings['general']['fields'][] = [
-            'name'     => 'registration-role',
+            'name'     => 'rua-registration-role',
             'title'    => __('New User Default Role'),
             'callback' => [$this,'setting_moved'],
             'args'     => [
-                'option' => !empty($roles[$default_role]) ? $roles[$default_role]['name'] : $default_role,
-                'title'  => __('General Settings'),
-                'url'    => 'options-general.php'
+                'option'   => !empty($roles[$default_role]) ? $roles[$default_role]['name'] : $default_role,
+                'wp_title' => __('General Settings'),
+                'url'      => 'options-general.php'
             ],
             'register' => false
         ];
 
         $this->settings['general']['fields'][] = [
-            'name'     => 'registration',
+            'name'     => 'rua-registration',
             'title'    => __('Enable Registration', 'restrict-user-access'),
             'callback' => [$this,'setting_moved'],
             'args'     => [
-                'option' => get_option('users_can_register') ? __('Yes') : __('No'),
-                'title'  => __('General Settings'),
-                'url'    => 'options-general.php'
+                'option'   => get_option('users_can_register') ? __('Yes') : __('No'),
+                'wp_title' => __('General Settings'),
+                'url'      => 'options-general.php'
             ],
             'register' => false
         ];
 
+        $this->settings['security']['fields'][] = [
+            'name'     => self::PREFIX . 'rest_api_access',
+            'title'    => __('REST API Content Protection', 'restrict-user-access'),
+            'callback' => [$this,'checkbox'],
+            'args'     => [
+                'default_value' => 1,
+                'recommended'   => __('Enabled'),
+                'description'   => __('Deny access to content in REST API for users without legitimate a purpose.', 'restrict-user-access') .
+                    ' <a target="_blank" rel="noopener" href="https://dev.institute/docs/restrict-user-access/faq/restricted-content-not-hidden/">' . __('Learn more') . '</a>'
+            ],
+        ];
         foreach ($this->settings as $section) {
             add_settings_section(
                 $this->prefix . $section['name'],
@@ -208,8 +235,10 @@ final class RUA_Settings_Page extends RUA_Admin
                 $this->slug
             );
             foreach ($section['fields'] as $field) {
+                $field['args']['title'] = $field['title'];
+                $field['args']['label_for'] = $field['name'];
                 add_settings_field(
-                    $this->prefix . $field['name'],
+                    $field['name'],
                     $field['title'],
                     $field['callback'],
                     $this->slug,
@@ -217,28 +246,10 @@ final class RUA_Settings_Page extends RUA_Admin
                     $field['args']
                 );
                 if (!isset($field['register']) || $field['register']) {
-                    register_setting($this->option_group, $this->prefix . $field['name']);
+                    register_setting($this->option_group, $field['name']);
                 }
             }
         }
-    }
-
-    /**
-     * Render levels dropdown
-     * Skip synchronized levels
-     *
-     * @since  0.17
-     * @param  array  $args
-     * @return void
-     */
-    public function dropdown_levels($args)
-    {
-        echo '<select name="' . $this->prefix . 'registration-level" id="' . $this->prefix . 'registration-level">';
-        echo '<option value="0">' . __('-- None --') . '</option>';
-        foreach (RUA_App::instance()->get_levels() as $id => $level) {
-            echo '<option value="' . $level->ID . '" ' . selected($level->ID, get_option($this->prefix . 'registration-level'), false) . '>' . $level->post_title . '</option>';
-        }
-        echo '</select>';
     }
 
     /**
@@ -250,8 +261,62 @@ final class RUA_Settings_Page extends RUA_Admin
      */
     public function checkbox($args)
     {
-        $option = get_option($args['label_for']);
-        echo '<input type="checkbox" name="' . $args['label_for'] . '" value="1" ' . checked($option, 1, 0) . '/>';
+        $option = $this->get_setting_value($args);
+
+        echo '<label class="cae-toggle">';
+        echo '<input type="checkbox" name="' . $args['label_for'] . '" value="1"' . checked($option, 1, 0) . '>';
+        echo '<div class="cae-toggle-bar"></div>';
+        echo '</label>';
+        if (isset($args['description'])) {
+            echo '<p class="description">' . $args['description'] . '</p>';
+        }
+        if (isset($args['recommended'])) {
+            echo '<p class="description">Recommended: <code>' . $args['recommended'] . '</code></p>';
+        }
+    }
+
+    /**
+     * @param $args
+     * @return void
+     */
+    public function radio($args)
+    {
+        $current_value = $this->get_setting_value($args);
+
+        echo '<fieldset>';
+        echo '<legend class="screen-reader-text">' . $args['title'] . '</legend>';
+        echo '<p>';
+        foreach ($this->get_options($args) as $option_value => $label) {
+            echo '<label>';
+            echo '<input type="radio" name="' . $args['label_for'] . '" value="' . $option_value . '"' . checked($current_value, $option_value, false) . '> ' . $label;
+            echo '</label><br />';
+        }
+        echo '</p>';
+        if (isset($args['description'])) {
+            echo '<p class="description">' . $args['description'] . '</p>';
+        }
+        if (isset($args['recommended'])) {
+            echo '<p class="description">Recommended: <code>' . $args['recommended'] . '</code></p>';
+        }
+        echo '</fieldset>';
+    }
+
+    /**
+     * @param $args
+     * @return void
+     */
+    public function dropdown($args)
+    {
+        $current_value = $this->get_setting_value($args);
+
+        echo '<select name="' . $args['label_for'] . '" id="' . $args['label_for'] . '">';
+        foreach ($this->get_options($args) as $option_value => $label) {
+            echo '<option value="' . $option_value . '" ' . selected($option_value, $current_value, false) . '>' . $label . '</option>';
+        }
+        echo '</select>';
+        if (isset($args['recommended'])) {
+            echo '<p class="description">Recommended: <code>' . $args['recommended'] . '</code></p>';
+        }
     }
 
     /**
@@ -266,7 +331,17 @@ final class RUA_Settings_Page extends RUA_Admin
         echo $args['option'];
         echo '<p class="description">' . sprintf(
             __('Setting can be changed in %s', 'restrict-user-access'),
-            '<a href="' . admin_url($args['url']) . '">' . $args['title'] . '</a>'
+            '<a href="' . admin_url($args['url']) . '">' . $args['wp_title'] . '</a>'
         ) . '</p>';
+    }
+
+    private function get_setting_value($args)
+    {
+        return get_option($args['label_for'], isset($args['default_value']) ? $args['default_value'] : false);
+    }
+
+    private function get_options($args)
+    {
+        return isset($args['options']) ? $args['options'] : [];
     }
 }
