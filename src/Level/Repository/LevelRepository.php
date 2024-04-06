492fe365832f12f5ec9c56ed39064ea6670b108e
Joachim Jensen <joachim@dev.institute>
2024-04-05 18:47:07 -0700

    [RUA-56] level repo

---
+++ src/Level/Repository/LevelRepository.php
@@ -0,0 +1,33 @@
+<?php
+namespace RestrictUserAccess\Level\Repository;
+
+use RestrictUserAccess\Repository\AbstractRepository;
+use RUA_Level;
+
+/**
+ * Class LevelRepository
+ *
+ * @author Joachim Jensen <joachim@dev.institute>
+ * @license https://www.gnu.org/licenses/gpl-3.0.html
+ */
+class LevelRepository extends AbstractRepository implements LevelRepositoryInterface
+{
+    public function find($id)
+    {
+        $wp_entity = \WP_Post::get_instance($id);
+        if (!($wp_entity instanceof \WP_Post)) {
+            return null;
+        }
+        return new RUA_Level($wp_entity);
+    }
+
+    /**
+     * @param $args
+     * @return \WP_Query
+     */
+    protected function query($args)
+    {
+        $args['post_type'] = \RUA_App::TYPE_RESTRICT;
+        return new \WP_Query($args);
+    }
+}
